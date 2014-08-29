ad19493be09432572a8d5203ad6d8edcb989d60f
William Budington <bill@eff.org>
2014-08-28 22:16:19 -0700

    Get size in bytes from user-provided argv

--- cpmusic.py
+++ cpmusic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import humanfriendly
 
 def usage():
     print sys.argv[0] + " <size> <source> <destination>"
@@ -7,3 +8,10 @@ def usage():
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         usage()
+        exit()
+    else:
+        try:
+            size = humanfriendly.parse_size(sys.argv[1])
+        except humanfriendly.InvalidSize:
+            usage()
+            exit()
