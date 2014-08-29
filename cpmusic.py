aa70ab8b51e7775b917d3e2390580b14d572c2f4
William Budington <bill@eff.org>
2014-08-28 22:53:56 -0700

    move functions into AlbumBuilder class

--- cpmusic.py
+++ cpmusic.py
@@ -10,24 +10,28 @@ def usage():
     print sys.argv[0] + " <size> <source> <destination>"
     print "Assumes the source descendents directory structure is <artist>/<album>/"
 
-def get_size(start_path = '.'):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return total_size
+class AlbumBuilder:
+    def __init__(self, source):
+        self.source = source
 
-def build_album_selection(start_path = '.'):
-    albums = []
-    for artist_name in os.listdir(start_path):
-        artist_path = os.path.join(start_path, artist_name)
-        if os.path.isdir(artist_path):
-            for album_name in os.listdir(artist_path):
-                album_path = os.path.join(artist_path, album_name)
-                if os.path.isdir(album_path):
-                    albums.append(album_path)
-    return albums
+    def get_size(start_path = '.'):
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(start_path):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                total_size += os.path.getsize(fp)
+        return total_size
+
+    def build_selection(self):
+        albums = []
+        for artist_name in os.listdir(self.source):
+            artist_path = os.path.join(self.source, artist_name)
+            if os.path.isdir(artist_path):
+                for album_name in os.listdir(artist_path):
+                    album_path = os.path.join(artist_path, album_name)
+                    if os.path.isdir(album_path):
+                        albums.append(album_path)
+        self.selection = albums
 
 
 if __name__ == "__main__":
@@ -40,4 +44,5 @@ if __name__ == "__main__":
         except humanfriendly.InvalidSize:
             usage()
             exit()
-        album_selection = build_album_selection(sys.argv[2])
+        builder = AlbumBuilder(sys.argv[2])
+        builder.build_selection()
